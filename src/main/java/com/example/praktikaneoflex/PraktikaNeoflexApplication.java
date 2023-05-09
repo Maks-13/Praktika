444a3ad1994e4baa6da791e74e710a9961ab536c
Maxim Yunaev <max01_13@mail.ru>
2023-05-09 14:12:52 +0300

    First0commit

---
+++ src/main/java/com/example/praktikaneoflex/PraktikaNeoflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.praktikaneoflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikaNeoflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraktikaNeoflexApplication.class, args);
+    }
+
+}
